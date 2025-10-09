11631b44ed84a093b791f63ed2e1e3c8ae0730ae
Fernando Choque <ferok_594@hotmail.com.ar>
2025-10-09 17:52:39 -0300

    dac FUNCIONANDO Y  adc (VER RESOLUCION)

---
+++ lab1/modules/adc/CTIMER0_GPIO00_DAC0_pruebaRampa/Debug/source/semihost_hardfault.d
@@ -0,0 +1,4 @@
+source/semihost_hardfault.o source/semihost_hardfault.d: \
+ ../source/semihost_hardfault.c \
+ C:\Users\ferok\Documents\MCUXpressoIDE_25.6.136\workspace_FRDMMCXN947\CTIMER0_GPIO00_DAC0_pruebaRampa\source\mcux_config.h
+C:\Users\ferok\Documents\MCUXpressoIDE_25.6.136\workspace_FRDMMCXN947\CTIMER0_GPIO00_DAC0_pruebaRampa\source\mcux_config.h:
---
+++ lab1/modules/adc/CTIMER0_GPIO00_DAC0_pruebaRampa/Debug/startup/startup_mcxn947_cm33_core0.d
@@ -0,0 +1,4 @@
+startup/startup_mcxn947_cm33_core0.o startup/startup_mcxn947_cm33_core0.d: \
+ ../startup/startup_mcxn947_cm33_core0.c \
+ C:\Users\ferok\Documents\MCUXpressoIDE_25.6.136\workspace_FRDMMCXN947\CTIMER0_GPIO00_DAC0_pruebaRampa\source\mcux_config.h
+C:\Users\ferok\Documents\MCUXpressoIDE_25.6.136\workspace_FRDMMCXN947\CTIMER0_GPIO00_DAC0_pruebaRampa\source\mcux_config.h:
